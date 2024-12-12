e8c81643499b8d7ba464c835698c9eb7e6064c1e
doxakis1 <mike.karat@yahoo.com>
2024-12-12 03:07:01 +0200

    Day7 done

--- Day7/part_two.c
+++ Day7/part_two.c
@@ -5,6 +5,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
 uint64_t ascii_to_uint64(const char *str) {
 	uint64_t result = 0;
 	while (*str >= '0' && *str <= '9')
@@ -44,27 +45,33 @@ uint64_t concatinate_nums(uint64_t num1, uint64_t num2)
 	return (num1 * multiplier) + num2;
 }
 
-uint64_t evaluate_answer(uint64_t *array, uint64_t combination, size_t arraySize)
+uint64_t evaluate_answer(uint64_t *array, char *combinationBuffer, size_t arraySize)
 {
 	uint64_t answer = array[0];
 	array++;
 	arraySize--;
 	for (size_t i = 0; i < arraySize; i++)
 	{
-		if (combination % 3)
-		{
-			if (answer == 48)
-				printf("%llu, %llu\n", answer, *array);
+		if (*combinationBuffer == 2)
 			answer = concatinate_nums(answer, *array);
-		}
-		else if (combination % 2)
+		else if (*combinationBuffer == 1)
 			answer += *array;
 		else
 			answer *= *array;
-		combination--;
+		combinationBuffer++;
 		array++;
 	}
 	return answer;
+}
+
+void make_base_three_string(uint64_t num, char *combinationBuffer)
+{
+	for (size_t i = 0; i < 1024; i++)
+	{
+		*combinationBuffer = num % 3;
+		num /= 3;
+		combinationBuffer++;
+	}
 }
 
 int64_t validate_line(char *buffer)
@@ -73,6 +80,7 @@ int64_t validate_line(char *buffer)
 	uint64_t	desiredAnswer = ascii_to_uint64(buffer);
 	size_t		arrayIndex = 0;
 	size_t		loopCounter = 0;
+	char		combinationBuffer[1024];
 
 	while (*buffer != ' ')
 		buffer++;
@@ -85,14 +93,11 @@ int64_t validate_line(char *buffer)
 		buffer++;
 	}
 	uint64_t	combination = power_three(arrayIndex - 1);
-	printf("Desired %llu, and combinations %llu\n", desiredAnswer, combination);
 	while(loopCounter <= combination)
 	{
-		if (evaluate_answer(array, loopCounter, arrayIndex) == desiredAnswer)
-		{
-			printf("%llu\n", desiredAnswer);
+		make_base_three_string(loopCounter, combinationBuffer);
+		if (evaluate_answer(array, combinationBuffer, arrayIndex) == desiredAnswer)
 			return desiredAnswer;
-		}
 		loopCounter++;
 	}
 	return 0;
