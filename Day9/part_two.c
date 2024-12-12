cc54b8ea1ab087b8755cdef1040819a16bdae8f7
doxakis1 <mike.karat@yahoo.com>
2024-12-12 08:32:11 +0200

    Day9 done

--- Day9/part_two.c
+++ Day9/part_two.c
@@ -9,7 +9,7 @@
 typedef struct s_node
 {
 	uint64_t	nodeID;
-	uint8_t		nodeSize;
+	int		nodeSize;
 	_Bool		hasBeenTested;
 	struct s_node	*next;
 } node;
@@ -27,9 +27,45 @@ size_t simple_get_line(int fd, char *buffer){
 	return index;
 }
 
+node *find_previous(node *head, node *desiredNode)
+{
+	node *tmp = head;
+	while (tmp && tmp->next != desiredNode)
+		tmp = tmp->next;
+	return tmp;
+}
+
+node *find_next_not_tested(node *head)
+{
+	node *previous = NULL;
+	node *tmp = head;
+	while (tmp != NULL)
+	{
+		if (tmp->nodeID != 0 && tmp->hasBeenTested == false)
+			previous = tmp;
+		tmp = tmp->next;
+	}
+	return previous;
+}
+
+
+node *find_next_fitting_place(node *head, node *testingNode)
+{
+	uint8_t	desiredSize = testingNode->nodeSize;
+	node *tmp = head;
+	while (tmp && tmp != testingNode)
+	{
+		if (tmp->nodeID == 0 && tmp->nodeSize >= desiredSize)
+			return tmp;
+		tmp = tmp->next;
+	}
+	return NULL;
+}
+
+
 int main(void){
 	char		readBuffer[30000] = {0};
-	node		nodeLList[30000] = {0};
+	node		nodeLList[80000] = {0};
 	uint64_t	constractedBuffer[300000] = {0};
 	uint64_t	answer = 0;
 	int fd = open("input.txt", O_RDONLY);
@@ -47,6 +83,7 @@ int main(void){
 			nodeLList[nodeIndex].nodeID = blockID;
 			nodeLList[nodeIndex].nodeSize = readBuffer[i] - '0';
 			nodeLList[nodeIndex].next = &nodeLList[nodeIndex + 1];
+			nodeLList[nodeIndex].hasBeenTested = false;
 			blockID++;
 		}
 		else
@@ -57,25 +94,43 @@ int main(void){
 		}
 		nodeIndex++;
 	}
-	nodeIndex--;
-	nodeLList[nodeIndex].next = NULL;
-	size_t end = constractedBufferIndex - 1;
-	for(size_t y = 0; y < constractedBufferIndex; y++)
+	nodeLList[nodeIndex - 1].next = NULL;
+	node *head = &nodeLList[0];
+	node *tmp = head;
+	head->hasBeenTested = true;
+	node *nextTest = find_next_not_tested(head);
+	node *fittingPlace = NULL;
+	while (nextTest)
 	{
-		if (constractedBuffer[y] == 0)
+		fittingPlace = find_next_fitting_place(head, nextTest);
+		if (fittingPlace)
 		{
-			while(end > y && constractedBuffer[end] == 0)
-				end--;
-			constractedBuffer[y] = constractedBuffer[end];
-			constractedBuffer[end] = 0;
-
+			nodeLList[nodeIndex].nodeID = 0;
+			nodeLList[nodeIndex].nodeSize = nextTest->nodeSize;
+			nodeLList[nodeIndex].next = nextTest->next;
+			find_previous(head, nextTest)->next = &nodeLList[nodeIndex];
+			find_previous(head, fittingPlace)->next = nextTest;
+			nextTest->next = fittingPlace;
+			fittingPlace->nodeSize -= nextTest->nodeSize;
+			if (fittingPlace->nodeSize == 0)
+				nextTest->next = fittingPlace->next;
+			nodeIndex++;
 		}
+		nextTest->hasBeenTested = true ;
+		nextTest = find_next_not_tested(head);
 	}
 	size_t index = 0;
-	while(constractedBuffer[index] != 0)
+	while (head)
 	{
-		answer += index * (constractedBuffer[index] - 1);
-		index++;
+		for (size_t i = 0; i < head->nodeSize; i++)
+		{
+			if (head->nodeID)
+			{
+				answer += index * (head->nodeID - 1);
+			}
+			index++;
+		}
+		head = head->next;
 	}
 	printf("The answer is: %llu", answer);
 	return close(fd);
